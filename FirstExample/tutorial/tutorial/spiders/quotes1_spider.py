f18c6b80daaba9045847206a59225d1d7fe86d72
Defa Wang <190870210@qq.com>
2017-11-18 11:52:16 +0800

    The basic use of Scrapy

---
+++ FirstExample/tutorial/tutorial/spiders/quotes1_spider.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class QuotesSpider1(scrapy.Spider):
+    name = "quotes1"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
